dca394b94afbc8e607b55ca887a49565319e2d12
Luís Henrique de Almeida <luis.h.wrk@gmail.com>
2026-03-08 02:45:25 -0300

    metalon: og image hotfix

--- src/routes/metalon/+page.svelte
+++ src/routes/metalon/+page.svelte
@@ -369,8 +369,7 @@ onMount(() => {
 <svelte:head>
 	<meta property="og:title" content="Luís Henrique Space - Tools | Metalon">
     <meta property="og:description" content="Ferramenta para orçamentos de metalon - Luís Henrique Space | Tools">
-	<meta property="og:image" content="https://luishenrique.space/metalon/api/imgval?{data.value? "value=" + data.value + "&" : ""}{data.name? "name=" + data.name : ""}">
-	<meta property="og:url" content="https://luishenrique.space/metalon">
+	<meta property="og:image" content="https://tools.luishenrique.space/metalon/api/imgval?{data.value? "value=" + data.value + "&" : ""}{data.name? "name=" + data.name : ""}">
 </svelte:head>
 
 
